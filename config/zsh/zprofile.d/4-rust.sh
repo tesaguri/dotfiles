2066a117873b5e5d414716bf97e27da946b3f31a
Daiki Mizukami <tesaguriguma@gmail.com>
2021-10-26 05:14:30 +0900

    cargo: Move `CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER` to `$CARGO_HOME/config.toml`

--- config/zsh/zprofile.d/4-rust.sh
+++ config/zsh/zprofile.d/4-rust.sh
@@ -2,6 +2,4 @@ export CARGO_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/cargo"
 export CARGO_INSTALL_ROOT="$HOME/.local"
 export CARGO_TARGET_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/cargo-target"
 
-export CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER=x86_64-linux-musl-ld
-
 export RUSTUP_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/rustup"
