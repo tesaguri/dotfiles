2392b199f666287ff7653814c41d8d35e51a6983
Daiki Mizukami <tesaguriguma@gmail.com>
2022-02-19 09:43:39 +0900

    nvim: Reorganize and extend config

--- config/nvim/init.d/config.lua
+++ config/nvim/init.d/config.lua
@@ -1,4 +1,15 @@
-vim.opt.colorcolumn = '101'
-vim.opt.clipboard = 'unnamedplus'
+-- Indent
+vim.opt.copyindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.softtabstop = -1
+
+-- Interface
+vim.opt.clipboard:append('unnamedplus')
 vim.opt.mouse = 'a'
+
+-- Appearance
+vim.opt.colorcolumn:append({ 101 })
 vim.opt.termguicolors = true
+vim.opt.winblend = 30
