07a09baac75757284830f0f0a8493b99748903b9
Daiki Mizukami <tesaguriguma@gmail.com>
2022-02-21 23:33:19 +0900

    nvim: Append `rust-src` to `'path'`

--- config/nvim/init.d/config.lua
+++ config/nvim/init.d/config.lua
@@ -13,3 +13,6 @@ vim.opt.mouse = 'a'
 vim.opt.colorcolumn:append({ 101 })
 vim.opt.termguicolors = true
 vim.opt.winblend = 30
+
+-- Add `rust-src` to `path`.
+vim.opt.path:append(io.popen('rustup show home'):read()..'/toolchains/'..string.match(io.popen('rustup show active-toolchain'):read(), '[^ ]+')..'/lib/rustlib/src/rust/library')
