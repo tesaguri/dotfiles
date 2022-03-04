3a5d9a785daee49025a3f43cd4b618c24cabdf15
Daiki Mizukami <tesaguriguma@gmail.com>
2022-03-04 12:06:14 +0900

    nvim: Finish some works undone in previous commits

--- hooks/post-up/dein.sh
+++ hooks/post-up/dein.sh
@@ -4,11 +4,12 @@ RCM_HOOK=1 . "$(dirname "$0")/../../config/zsh/.zprofile" "$@"
 main() {
 	unset -f main
 
-	local dein_repo="${XDG_CACHE_HOME:-$HOME/.cache}/dein/repos/github.com/Shougo/dein.vim"
+	local dein_repo
+	dein_repo="$(nvim --headless -u NONE +'echo stdpath("cache")' +q 2>&1)/dein/repos/github.com/Shougo/dein.vim"
 	if ! [ -d "$dein_repo" ]; then
 		mkdir -p "$(dirname "$dein_repo")"
 		git clone 'https://github.com/Shougo/dein.vim.git' "$dein_repo"
-		nvim -c 'try | if dein#check_install() | call dein#install() | endif | finally | qall | endtry'
+		nvim --headless -c 'try | if dein#check_install() | call dein#install() | endif | finally | qall | endtry'
 	fi
 }
 
