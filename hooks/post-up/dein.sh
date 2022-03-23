81521b0c52a8ffe646e3e8cab55364aacec9821e
Daiki Mizukami <tesaguriguma@gmail.com>
2022-03-24 08:54:19 +0900

    nvim: Refactor rcm hook for dein
    The `if dein#check_install()` block was not necessary as we've just
    cloned the repository before that line.

--- hooks/post-up/dein.sh
+++ hooks/post-up/dein.sh
@@ -1,16 +1,14 @@
+#!/usr/bin/env zsh
+
 # shellcheck disable=SC1090
 RCM_HOOK=1 . "$(dirname "$0")/../../config/zsh/.zprofile" "$@"
 
-main() {
-	unset -f main
-
+function() {
 	local dein_repo
 	dein_repo="$(nvim --headless -u NONE +'echo stdpath("cache")' +q 2>&1)/dein/repos/github.com/Shougo/dein.vim"
 	if ! [ -d "$dein_repo" ]; then
 		mkdir -p "$(dirname "$dein_repo")"
 		git clone 'https://github.com/Shougo/dein.vim.git' "$dein_repo"
-		nvim --headless -c 'try | if dein#check_install() | call dein#install() | endif | finally | qall | endtry'
+		nvim --headless -c 'try | call dein#install() | finally | qall | endtry'
 	fi
 }
-
-main "$@"
