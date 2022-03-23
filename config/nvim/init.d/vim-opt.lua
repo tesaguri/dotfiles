93d1a3043ed23dd11917f33c367af9c94b8edde2
Daiki Mizukami <tesaguriguma@gmail.com>
2022-03-24 08:46:50 +0900

    nvim: Fix format

--- config/nvim/init.d/vim-opt.lua
+++ config/nvim/init.d/vim-opt.lua
@@ -5,7 +5,7 @@ vim.opt.mouse = "a"
 -- Appearance
 vim.opt.colorcolumn:append { 101 }
 vim.opt.cursorline = true
-vim.opt.cursorlineopt = { 'number' }
+vim.opt.cursorlineopt = { "number" }
 vim.opt.number = true
 vim.opt.termguicolors = true
 vim.opt.winblend = 30
