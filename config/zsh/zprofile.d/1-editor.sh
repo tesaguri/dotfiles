91114f0b9b5424a2feb49173aadef48529efdb8a
Daiki Mizukami <tesaguriguma@gmail.com>
2023-10-19 22:32:26 +0900

    zsh: Make start up script work even when commands are missing

--- config/zsh/zprofile.d/1-editor.sh
+++ config/zsh/zprofile.d/1-editor.sh
@@ -1,1 +1,5 @@
-export EDITOR=nvim
+if command -v nvim > /dev/null; then
+	export EDITOR=nvim
+elif command -v vim > /dev/null; then
+	export EDITOR=vim
+fi
--- config/zsh/zshrc.d/gpg.sh
+++ config/zsh/zshrc.d/gpg.sh
@@ -1,9 +1,13 @@
-GPG_TTY="$(tty)"
+if ! command -v gpgconf > /dev/null; then
+	return
+fi
+
+GPG_TTY="${TTY:-$(tty)}"
 export GPG_TTY
 SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 export SSH_AUTH_SOCK
 
-gpgconf --launch gpg-agent
-
-eval "ssh() { gpg-connect-agent updatestartuptty /bye > /dev/null && $(whence -p ssh) \"\$@\"; }"
-eval "scp() { gpg-connect-agent updatestartuptty /bye > /dev/null && $(whence -p scp) \"\$@\"; }"
+if gpgconf --launch gpg-agent 2> /dev/null; then
+	eval "ssh() { gpg-connect-agent updatestartuptty /bye > /dev/null && $(whence -p ssh) \"\$@\"; }"
+	eval "scp() { gpg-connect-agent updatestartuptty /bye > /dev/null && $(whence -p scp) \"\$@\"; }"
+fi
