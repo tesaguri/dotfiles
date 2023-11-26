5e2d5318b2a292b48024f4064b226f7a88952b5e
Daiki Mizukami <tesaguriguma@gmail.com>
2023-11-26 12:40:02 +0900

    zsh: Define wrappers in `gpg.sh` more properly

--- config/zsh/zshrc.d/gpg.sh
+++ config/zsh/zshrc.d/gpg.sh
@@ -8,6 +8,12 @@ SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 export SSH_AUTH_SOCK
 
 if gpgconf --launch gpg-agent 2> /dev/null; then
-	eval "ssh() { gpg-connect-agent updatestartuptty /bye > /dev/null && $(whence -p ssh) \"\$@\"; }"
-	eval "scp() { gpg-connect-agent updatestartuptty /bye > /dev/null && $(whence -p scp) \"\$@\"; }"
+	# Redefine `ssh` and `scp` commands to update TTY of the agent, if the commands exist and are
+	# are not already overriden. cf. `--enable-ssh-support` in `gpg-agent(1)`.
+	if command -v ssh > /dev/null && [ "$(command -v ssh)" = "$(whence -p ssh)" ]; then
+		eval "ssh() { gpg-connect-agent updatestartuptty /bye > /dev/null && $(printf '%q' "$(whence -p ssh)") \"\$@\"; }"
+	fi
+	if command -v scp > /dev/null && [ "$(command -v scp)" = "$(whence -p scp)" ]; then
+		eval "scp() { gpg-connect-agent updatestartuptty /bye > /dev/null && $(printf '%q' "$(whence -p scp)") \"\$@\"; }"
+	fi
 fi
