aae3b0a61a2d9d1d70f0ff3c13e5b0ab8f600213
Daiki Mizukami <tesaguriguma@gmail.com>
2023-11-26 12:52:28 +0900

    zsh: Tweak definition of `$SSH_AUTH_SOCK` according to `gpg-agent(1)`

--- config/zsh/zshrc.d/gpg.sh
+++ config/zsh/zshrc.d/gpg.sh
@@ -4,8 +4,13 @@ fi
 
 GPG_TTY="${TTY:-$(tty)}"
 export GPG_TTY
-SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
-export SSH_AUTH_SOCK
+
+# Use `gpg-agent` as an SSH agent. cf. EXAMPLES of `gpg-agent(1)`.
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+	SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)" &&
+	export SSH_AUTH_SOCK
+fi
 
 if gpgconf --launch gpg-agent 2> /dev/null; then
 	# Redefine `ssh` and `scp` commands to update TTY of the agent, if the commands exist and are
