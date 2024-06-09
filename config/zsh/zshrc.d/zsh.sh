b8d61c4365ac54e536e739b78431838bb07240f2
Daiki Mizukami <tesaguriguma@gmail.com>
2024-06-09 18:06:22 +0900

    zsh: Update $PROMPT to display signal name along with exit status

--- config/zsh/zshrc.d/zsh.sh
+++ config/zsh/zshrc.d/zsh.sh
@@ -7,7 +7,46 @@ function() {
 
 	bindkey -e
 
-	export PROMPT='%0(?.%F{green}✔.%130(?.%F{yellow}⚠[%?].%F{red}✘[%?]))%f%n@%m%# '
+	# Display exit status and optional signal name in `PROMPT`:
+	local prompt_signal signal_name exit_status=$((128 + 31))
+	for signal_name in \
+		USR2 \
+		USR1 \
+		INFO \
+		WINCH \
+		PROF \
+		VTALRM \
+		XFSZ \
+		XCPU \
+		IO \
+		TTOU \
+		TTIN \
+		CHLD \
+		CONT \
+		TSTP \
+		STOP \
+		URG \
+		TERM \
+		ALRM \
+		PIPE \
+		SYS \
+		SEGV \
+		BUS \
+		KILL \
+		FPE \
+		EMT \
+		ABRT \
+		TRAP \
+		ILL \
+		QUIT \
+		INT \
+		HUP
+	do
+		prompt_signal="%$exit_status(?.($signal_name).$prompt_signal)"
+		exit_status=$((exit_status - 1))
+	done
+	export PROMPT="%(?.%F{green}0.%F{red}%?$prompt_signal)%f:%n@%m%# "
+
 	# Show working directory in grey color.
 	export RPROMPT='%F{242}%~'
 }
